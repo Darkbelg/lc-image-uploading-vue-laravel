bb0c824034635343c3b0bc010fd54a8cb0045c3e
Darkbelg <stijn.sagaert@outlook.com>
2022-04-21 14:08:11 +0200

    Episode 4 Into The Pond

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,13 +1,39 @@
 <template>
-    <div>
-        <h2 class="text-xl text-center">{{ message }}</h2>
+    <div class="mt-4">
+        <file-pond
+        name="image"
+        ref="pond"
+        label-idle="Click to choose image, or drag here..."
+        server="/upload"
+        @init="filepondInitialized"
+        accepted-file-types="image/*"
+        />
+<!--        :allow-browse="false"-->
+
     </div>
 </template>
 <script>
+import vueFilePond from 'vue-filepond';
+import "filepond/dist/filepond.min.css";
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+
+const FilePond = vueFilePond(FilePondPluginFileValidateType);
+
 export default {
     data() {
         return {
-            message: 'Hello, world!'
+        }
+    },
+    components: {
+        // it is possible to change the component name
+        // 'vue-filepond' : FilePond
+        // Automatically goes to lowercase and adds - to capital letters except the first one
+        FilePond,
+    },
+    methods: {
+        filepondInitialized(){
+            console.log('Filepond is ready!');
+            console.log('Filepond object:', this.$refs.pond);
         }
     }
 }
